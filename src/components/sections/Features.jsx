e5561bd23003204620aba42af01908eca6cde41a
Moran <thaler.moran@gmail.com>
2023-03-09 10:58:56 +0100

    Add the functionalities to the navbar and organize the components

---
+++ src/components/sections/Features.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './features.style.scss';
+import Feature from '../layouts/Feature';
+
+import featureDish1 from "../../assets/images/feature_dish1@2x.png";
+import featureDish2 from "../../assets/images/feature_dish2@2x.png";
+
+function Features() {
+  return (
+    <section >
+      <Feature
+        header="about us"
+        text="“Viridis et Fortis” means green and strong.
+      The point of this extraordinary establishment
+      is to open th eyes of people (both vegan and
+      non-vegan) and to also introduce their taste
+      buds to a plethora of intricate flavours and
+      nutritional benefits of the vegan food"
+        img={featureDish1}
+        id="about"
+      />
+      <Feature
+        header="why us"
+        text="“Viridis et Fortis” is dedicated to saving the planet by suggesting you go green, and the way we’re going right now, we think we are achieving that one convincing plate at a time. We are not in anyway trying to force non-vegans out of their meat eating culture, what we are doing is helping them realize that vegetables equally have the qualities meat have. We give you a plate and you choose for yourself."
+        img={featureDish2}
+        className="left-align"
+        id="why"
+      />
+    </section>
+  );
+}
+
+export default Features;
---
+++ src/components/sections/MeetChef.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './meetChef.style.scss';
+
+function MeetChef(props) {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default MeetChef;
